fb6506bb7d07a02982347a40502a2fb1847e5501
llj <Kosovo.jy@qq.com>
2021-01-07 10:08:57 +0800

    make a new springboot project to test its features

---
+++ src/main/java/com/llj/springbootjap/SpringbootJapTestApplication.java
@@ -0,0 +1,13 @@
+package com.llj.springbootjap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJapTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJapTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llj/springbootjap/SpringbootJapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llj.springbootjap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
